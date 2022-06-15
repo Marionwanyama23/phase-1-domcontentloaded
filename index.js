05e0feb0be9261e1767de9850f81fa9086b81741
+ <+>
2022-06-15 23:32:16 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-document.addEventListener( "DOMContentLoaded", function () {
-    updateDOM()
+document.addEventListener( "DOMContentLoaded",  () => {
+    const newTaskForm = document.getElementById("create-task-form");
+
+    const newTaskDescription = document.getElementById("new-task-description");
+
+    const newTaskPriority = document.getElementById("new-task-priority");
+
+    const newTaskUl = document.getElementById("tasks");
+
+    newTaskForm.addEventListener("submit", createNewTask);
 });
 
-function updateDOM() {
-    document.getElementById("text")
-    .innerHTML = "This is really cool!";
-}
+const createNewTask = event => {
+    event.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description");
+
+    const newTask = document.createElement("li");
+
+    newTask.innerText = newTaskDescription.value;
+
+    appendNewTask(newTask);
+
+    event.target.reset();
+};
+const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+};
